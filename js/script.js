61442835963e3d85497523b6e9fb6c740f877782
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-04-30 20:30:55 -0300

    att nome site e mensagem sem produto

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 // Função para carregar os produtos contidos no db.json
 async function loadProducts() {
-    const response = await fetch('http://localhost:3000/produtos');
-    const data = await response.json();
-    const productsContainer = document.querySelector('.cards-produtos')
-    
-    data.forEach(product => {
-        const card = `
-                <div class="card" data-id="${product.id}">
-                    <img class="card__img" src="${product.imagem}" alt="${product.nome}">
-                    <div class="card-container__info">
-                        <div class="card-container__nome">
-                            <p>${product.nome}</p>
-                        </div>
-                        <div class="card-container__valor">
-                            <p>R$ ${product.preco}</p>
-                            <img class="icone-trash" src="./images/trash.png" alt="icone lixeira" onclick="deleteProduct(${product.id})">
+    try {
+        const response = await fetch('http://localhost:3000/produtos');
+        const data = await response.json();
+        const productsContainer = document.querySelector('.cards-produtos')
+        const mensagemSemProduto = document.querySelector('.semProdutos');
+        
+        if (data.length === 0) {
+            mensagemSemProduto.style.display = 'block';
+        } else {
+            mensagemSemProduto.style.display = 'none';
+        }
+        
+        data.forEach(product => {
+            const card = `
+                    <div class="card" data-id="${product.id}">
+                        <img class="card__img" src="${product.imagem}" alt="${product.nome}">
+                        <div class="card-container__info">
+                            <div class="card-container__nome">
+                                <p>${product.nome}</p>
+                            </div>
+                            <div class="card-container__valor">
+                                <p>R$ ${product.preco}</p>
+                                <img class="icone-trash" src="./images/trash.png" alt="icone lixeira" onclick="deleteProduct(${product.id})">
+                            </div>
                         </div>
                     </div>
-                </div>
-            `;
-                productsContainer.insertAdjacentHTML('beforeend', card);
-    });
+                `;
+                    productsContainer.insertAdjacentHTML('beforeend', card);
+        });
+    } 
+    catch (error) {
+        console.log(`Erro ao carregar produto: ${error.message}`);
+    }
 }
 
 // Função para adicionar um novo produto
@@ -31,34 +43,44 @@ async function addProduct(event) {
     const productPreco = form.elements['preco'].value;
     const productImagem = form.elements['link'].value;
 
-    const response = await fetch('http://localhost:3000/produtos', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            nome: productNome,
-            preco: productPreco,
-            imagem: productImagem
-        })
-    });
+    try {
+        const response = await fetch('http://localhost:3000/produtos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nome: productNome,
+                preco: productPreco,
+                imagem: productImagem
+            })
+        });
 
-    if (response.ok) {
-        form.reset();
-        loadProducts();
-    }
+        if (response.ok) {
+            form.reset();
+            loadProducts();
+        }
+    } 
+    catch (error) {
+        console.log(`Erro ao adicionar produto: ${error.message}`);
+    }    
 }
 
 async function deleteProduct(productId) {
-    // Fazendo uma busca direta no endpoint produtos, pelo numero ID. Como ele é unico por produto, assim que achar esse ID, será deletado com o metodo DELETE.
-    const response = await fetch(`http://localhost:3000/produtos/${productId}`, {
-        method: 'DELETE'
-    });
+    try {
+        // Fazendo uma busca direta no endpoint produtos, pelo numero ID. Como ele é unico por produto, assim que achar esse ID, será deletado com o metodo DELETE.
+        const response = await fetch(`http://localhost:3000/produtos/${productId}`, {
+            method: 'DELETE'
+        });
 
-    // Se a resposta foi bem sucedida, significa que o item foi deletado corretamente, sendo assim, agora vamos selecionar no HTML o campo div card que corresponde a esse ID e remover essa div com id respectivo
-    if (response.ok) {
-        const productCard = document.querySelector(`[data-id="${productId}"]`);
-        productCard.remove();
+        // Se a resposta foi bem sucedida, significa que o item foi deletado corretamente, sendo assim, agora vamos selecionar no HTML o campo div card que corresponde a esse ID e remover essa div com id respectivo
+        if (response.ok) {
+            const productCard = document.querySelector(`[data-id="${productId}"]`);
+            productCard.remove();
+        }
+    } 
+    catch (error) {
+        console.log(`Erro ao deletar produto: ${error.message}`);
     }
 
 }
