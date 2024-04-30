ec37246518d1eb95d4ef5eab7dfd26fee2e0be61
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-04-30 17:50:21 -0300

    Criei função para carregar produtos do db.json

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-fetch('http://localhost:3000/produtos')
-.then( response => {
-    console.log('teste', response);
-    const data = response.json();
-    console.log(typeof data);
-    data.map(i => {console.log(i.nome)})
-})
+// Função para carregar os produtos contidos no db.json
+async function loadProducts() {
+    const response = await fetch('http://localhost:3000/produtos');
+    const data = await response.json();
+    const productsContainer = document.querySelector('.cards-produtos')
+    
+    data.forEach(product => {
+        const card = `
+                <div class="card" data-id="${product.id}">
+                    <img class="card__img" src="${product.imagem}" alt="${product.nome}">
+                    <div class="card-container__info">
+                        <div class="card-container__nome">
+                            <p>${product.nome}</p>
+                        </div>
+                        <div class="card-container__valor">
+                            <p>R$ ${product.preco}</p>
+                            <img class="icone-trash" src="./images/trash.png" alt="icone lixeira" onclick="deleteProduct(${product.id})">
+                        </div>
+                    </div>
+                </div>
+            `;
+                productsContainer.insertAdjacentHTML('beforeend', card);
+    });
+}
+
+loadProducts(); 
+
+
